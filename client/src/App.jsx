392680d417f52d8b3a6fa3b3cbd5a95f30796586
kwang99 <asd45618@naver.com>
2024-05-27 17:14:41 +0900

    settings

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "@/Layout";
+import LoginView from "./views/LoginView";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/login" element={<LoginView />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
