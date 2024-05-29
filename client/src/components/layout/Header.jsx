16e264ce3e7e10e50602e1d839dcf8ba6cdcd4ad
unknown <tjdghqkr0615@gmail.com>
2024-05-29 16:57:22 +0900

    2024-05-29 16시57분 정보수정 링크 추가

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,100 +1,96 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import cn from "classnames";
-import Logoimage from '@/assets/image/logo.png'
+import Logoimage from "@/assets/image/logo.png";
 import Nav from "@/components/layout/Nav.jsx";
 
-
-
-
 const HeaderBlock = styled.div`
-background: var(--main);
-padding: 30px 15px;
-color: white;
-.wrap{
-  display: flex;
-justify-content: space-between;
-align-items: center;
-}
-.logo {
-      width: 120px;
-      img {
-        width: 100%;
-        height: auto;
-      }
-      position: relative;
-      z-index: 9999999;
-    }
-
-.menu-wrap {
-      position: relative;
-      width: 28px;
-      height: 24px;
-      cursor: pointer;
-      z-index: 9999999;
-    }
-    .menu-wrap .line {
-      position: absolute;
+  background: var(--main);
+  padding: 30px 15px;
+  color: white;
+  .wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo {
+    width: 120px;
+    img {
       width: 100%;
-      height: 3px;
-      border-radius: 2px;
-      background: #fff;
-      left: 0;
+      height: auto;
     }
-    .menu-wrap .line:first-child {
-      top: 0;
-      transform-origin: 25% 50%;
-      transition: 0.3s;
-    }
-    .menu-wrap .line:nth-child(2) {
-      top: calc(50% - 1.5px);
-    }
-    .menu-wrap .line:last-child {
-      bottom: 0;
-      transform-origin: 25% 50%;
-      transition: 0.3s;
-    }
+    position: relative;
+    z-index: 9999999;
+  }
 
-    .menu-wrap.open .line:first-child {
-      transform: rotate(45deg) translateX(25%);
-    }
-    .menu-wrap.open .line:nth-child(2) {
-      opacity: 0;
-    }
-    .menu-wrap.open .line:last-child {
-      transform: rotate(-45deg) translateX(25%);
-    }
-`
+  .menu-wrap {
+    position: relative;
+    width: 28px;
+    height: 24px;
+    cursor: pointer;
+    z-index: 9999999;
+  }
+  .menu-wrap .line {
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    border-radius: 2px;
+    background: #fff;
+    left: 0;
+  }
+  .menu-wrap .line:first-child {
+    top: 0;
+    transform-origin: 25% 50%;
+    transition: 0.3s;
+  }
+  .menu-wrap .line:nth-child(2) {
+    top: calc(50% - 1.5px);
+  }
+  .menu-wrap .line:last-child {
+    bottom: 0;
+    transform-origin: 25% 50%;
+    transition: 0.3s;
+  }
+
+  .menu-wrap.open .line:first-child {
+    transform: rotate(45deg) translateX(25%);
+  }
+  .menu-wrap.open .line:nth-child(2) {
+    opacity: 0;
+  }
+  .menu-wrap.open .line:last-child {
+    transform: rotate(-45deg) translateX(25%);
+  }
+`;
 
 const Header = () => {
-  const [isOpen,setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  
-  return <HeaderBlock>
-    <div className="wrap">
-          <h1 className="logo">
+
+  return (
+    <HeaderBlock>
+      <div className="wrap">
+        <h1 className="logo">
           <Link to="/">
             <img src={Logoimage} alt="로고이미지" />
           </Link>
-          </h1>
-          <div
-            className={cn("menu-wrap", isOpen ? "open" : "")}
-            onClick={toggleMenu}
-          >
-            <span className="line"></span>
-            <span className="line"></span>
-            <span className="line"></span>
-          </div>
-          </div>
-          <Nav isOpen={isOpen} toggleMenu={toggleMenu}/>
-        </HeaderBlock>;
-          
-
-       
-};    
+        </h1>
+        <div
+          className={cn("menu-wrap", isOpen ? "open" : "")}
+          onClick={toggleMenu}
+        >
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+      <Nav isOpen={isOpen} toggleMenu={toggleMenu} />
+    </HeaderBlock>
+  );
+};
 
 export default Header;
--- client/src/components/layout/Nav.jsx
+++ client/src/components/layout/Nav.jsx
@@ -1,216 +1,284 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import {Link, NavLink} from 'react-router-dom'
-import cn from 'classnames';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link, NavLink } from "react-router-dom";
+import cn from "classnames";
 // import Dropdown from 'react-bootstrap/Dropdown';
-import Accordion from 'react-bootstrap/Accordion';
-import { CiFacebook,CiInstagram,CiTwitter  } from "react-icons/ci";
-import { IoIosArrowForward,IoIosArrowDown } from "react-icons/io";
-
+import Accordion from "react-bootstrap/Accordion";
+import { CiFacebook, CiInstagram, CiTwitter } from "react-icons/ci";
+import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
+import { FaUser, FaHeart, FaBowlFood } from "react-icons/fa6";
 
 const NavBlock = styled.div`
-font-family: "Do Hyeon", sans-serif;
-font-weight: 400;
-font-style: normal;
-position: fixed;
-top: 0;
-right: -768px;
-width: 100%;
-height: 100%;
-background:var(--main);
-opacity: 0;
-transition: all 0.6s ease;
-overflow: hidden;
-&.on{
-right:0;
-opacity: 1;
-}
-.nav__wrap{
-    padding: 100px 15px 0px;
-    .nav__depth1{
-        padding: 0;
-        margin: 0;
-        padding-bottom: 10px;
-        li{
-          
-            span{
-                font-size:1em;
-            }
-            a{
-                display: flex;
-            align-items: center;
-            justify-content: space-between;
-            font-family: "Do Hyeon", sans-serif;
-            font-weight: 400;
-            font-style: normal;
-            font-size: 1.7em;
-            letter-spacing: 1px;
-            padding: 15px 0;
-            }
-        }
-        .myPage{
-          
-            font-size: 1.7em;
-            letter-spacing: 1px;
-            padding: 15px 0;
-            overflow: hidden;
-            cursor: pointer;
-            span{
-                display: flex;
-            align-items: center;
-            justify-content: space-between;
-            
-            }
-            .arrow{
-                transition: all 0.5s ease;
-            &.on{
-                transform: rotate(180deg);
-            }
-        }
-          
-            .nav__depth2 {
-  max-height: 0;
+  font-family: "Do Hyeon", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  position: fixed;
+  top: 0;
+  right: -768px;
+  width: 100%;
+  height: 100%;
+  background: var(--main);
   opacity: 0;
-  overflow: hidden;
-  transition: all 0.5s ease;
-  transform: scaleY(0);
-  transform-origin: top;
-  li{
-    a{
-        font-size: 0.7em;
+  transition: all 0.6s ease;
+  overflow: auto;
+  z-index: 9999;
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: white;
+    height: 5px;
+    padding: 0 10px;
+    border-radius: 20px;
+    margin: 0 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: var(--main);
+  }
+  &.on {
+    right: 0;
+    opacity: 1;
+  }
+  .nav__wrap {
+    padding: 100px 15px 0px;
+    .nav__depth1 {
+      padding: 0;
+      margin: 0;
+      border-bottom: 1px solid #fff;
+      padding-bottom: 50px;
+
+      li {
+        span {
+          font-size: 1em;
+        }
+        a {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          font-family: "Do Hyeon", sans-serif;
+          font-weight: 400;
+          font-style: normal;
+          font-size: 1.7em;
+          letter-spacing: 1px;
+          padding: 15px 0;
+        }
+      }
+      .myPage {
+        font-size: 1.7em;
+        letter-spacing: 1px;
+        padding: 15px 0;
+        overflow: hidden;
+        cursor: pointer;
+
+        span {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+        .arrow {
+          transition: all 0.5s ease;
+          &.on {
+            transform: rotate(-180deg);
+          }
+        }
+
+        .nav__depth2 {
+          max-height: 0;
+          opacity: 0;
+          overflow: hidden;
+          transition: all 0.5s ease;
+          transform: scaleY(0);
+          transform-origin: top;
+          li {
+            a {
+              font-size: 0.8em;
+              span {
+                font-size: 0.9em;
+                padding-right: 2px;
+              }
+            }
+          }
+        }
+
+        .nav__depth2.on {
+          max-height: 300px;
+          opacity: 1;
+          transform: scaleY(1);
+        }
+      }
+    }
+    .Family__site {
+      .Family__name {
+        font-size: 2em;
+        padding-top: 30px;
+      }
+      .Family__list {
+        .accordion {
+          padding-top: 50px;
+        }
+        .accordion-button collapsed {
+          border: none;
+          margin-bottom: 20px;
+          margin-top: 50px;
+        }
+        .accordion-button {
+          background: white;
+          border: none;
+          font-size: 1em;
+
+          color: var(--main);
+        }
+        .accordion-body {
+          transition: all 0.5s;
+          &:hover {
+            background: #f3f3f3;
+            a {
+              color: var(--main);
+            }
+          }
+          a {
+            font-size: 1.2em;
+            transition: all 0.3s;
+          }
+        }
+      }
+    }
+    .sns {
+      display: flex;
+      justify-content: right;
+      margin: 50px 0;
+      a {
+        display: inline-block;
+        font-size: 3em;
+        &.hover {
+          background: green;
+        }
+      }
     }
   }
-}
+`;
 
-.nav__depth2.on {
-  max-height: 300px; 
-  opacity: 1;
-  transform: scaleY(1);
-}
-        }
-    }
-    .Family__site{
-        .Family__name{
-            font-size:2em;
-            padding-top: 30px;
-        }
-        .Family__list{
-            #dropdown-basic{
-                width: 100%;
-                background: var(--main);
-                border: 1px solid #fff;
-                font-size: 1.3em;
-                text-align: left;
-                padding: 20px 10px;
+const Nav = ({ isOpen, toggleMenu }) => {
+  const [depth2, setDepth2] = useState(false);
 
-            }
-            .accordion{
-            }
-            .accordion-button collapsed{
-                border:none;
-                margin-bottom: 20px;
-            }
-            .accordion-button{
-                background: white;
-                border: none;
-                font-size: 1em;
-                color: var(--main);
-            }
-            .accordion-body{
-                transition: all 0.5s;
-                &:hover{
-                    background: #f3f3f3;
-                    a{
-                        color: var(--main);
-                    }
-                }
-                a{
-                    font-size: 1.2em;
-                    transition: all 0.3s;
-                }
-            }
-              
-        }
-    }
-    .sns{
-        display: flex;
-        justify-content: right;
-        margin-bottom: 30px;
-        a{
-            display: inline-block;
-            font-size: 3em;
-            &.hover{
-                background: green;
-            }
-        }
-    }
-}
+  const depth2Click = () => {
+    setDepth2(!depth2);
+  };
 
-`
+  return (
+    <NavBlock className={isOpen ? "on" : ""}>
+      <div className="nav__wrap">
+        <ul className="nav__depth1">
+          <li onClick={toggleMenu}>
+            <NavLink to="/">
+              회사소개{" "}
+              <span>
+                <IoIosArrowForward />
+              </span>
+            </NavLink>
+          </li>
+          <li onClick={toggleMenu}>
+            <NavLink to="/">
+              서비스{" "}
+              <span>
+                <IoIosArrowForward />
+              </span>
+            </NavLink>
+          </li>
+          <li onClick={toggleMenu}>
+            <NavLink to="/">
+              오시는길{" "}
+              <span>
+                <IoIosArrowForward />
+              </span>
+            </NavLink>
+          </li>
+          <li className="myPage" onClick={depth2Click}>
+            {" "}
+            <span>
+              마이페이지
+              <IoIosArrowDown className={cn("arrow", depth2 ? "on" : "")} />
+            </span>
+            <ul className={cn("nav__depth2", depth2 ? "on" : "")}>
+              <li onClick={toggleMenu}>
+                <NavLink to="/memberModify">
+                  정보수정
+                  <span>
+                    <FaUser />
+                  </span>
+                </NavLink>
+              </li>
+              <li onClick={toggleMenu}>
+                <NavLink to="/">
+                  좋아요 리스트
+                  <span>
+                    <FaHeart />
+                  </span>
+                </NavLink>
+              </li>
+              <li onClick={toggleMenu}>
+                <NavLink to="/">
+                  최근 본 음식
+                  <span>
+                    <FaBowlFood />
+                  </span>
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div className="Family__site">
+          <div className="Family__list">
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>FamilySite</Accordion.Header>
 
-const Nav = ({isOpen, toggleMenu}) => {
-
-
-    const [depth2,setDepth2] = useState(false)
-
-    const depth2Click = ()=>{
-        setDepth2(!depth2)
-    } 
-
-
-    return (
-        <NavBlock className={isOpen ? 'on' : ''}>
-            <div className="nav__wrap">
-                <ul className="nav__depth1">
-                    <li onClick={toggleMenu}><NavLink to="/">회사소개 <span><IoIosArrowForward/></span></NavLink></li>
-                    <li onClick={toggleMenu}><NavLink to="/">서비스 <span><IoIosArrowForward/></span></NavLink></li>
-                    <li onClick={toggleMenu}><NavLink to="/">오시는길 <span><IoIosArrowForward/></span></NavLink></li>
-                    <li className='myPage' onClick={depth2Click}> <span>마이페이지<IoIosArrowDown className={cn('arrow', depth2 ? 'on' : '')}/></span>
-                    <ul className={cn('nav__depth2', depth2 ? 'on' : '' )}> 
-                            <li onClick={toggleMenu}><NavLink to="/">정보수정</NavLink></li>
-                            <li onClick={toggleMenu}><NavLink to="/">좋아요 리스트</NavLink></li>
-                            <li onClick={toggleMenu}><NavLink to="/">최근 본 음식</NavLink></li>
-                        </ul>
-                    </li>
-                </ul>
-                <div className="Family__site">
-                    <div className="Family__list">
-                    
-                    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>FamilySite</Accordion.Header>
-      
-        <Accordion.Body>
-            <a href="https://www.woowahan.com/" target='_blank'>우아한형제들</a>
-        </Accordion.Body>
-        <Accordion.Body>
-        <a href="https://www.deliveryhero.com/" target='_blank'>deliveryhero</a>
-        </Accordion.Body>
-        <Accordion.Body>
-        <a href="https://www.woowayouths.com/" target='_blank'>우아한청년들</a>
-        </Accordion.Body>
-        <Accordion.Body>
-        <a href="https://woowarider.or.kr/" target='_blank'>우아한 라이더 살핌기금</a>
-        </Accordion.Body>
-        <Accordion.Body>
-        <a href="https://woowasajangnim.or.kr/" target='_blank'>우아한 사장님 자녀 장학금 지원</a>
-        </Accordion.Body>
-        <Accordion.Body>
-        <a href="https://baemin.dosirak.or.kr/">배민방학도시락</a>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-                    </div>
-                </div>
-                <div className="sns">
-                <a href='' className='facebook'><CiFacebook/></a>
-                <a href='' className='instargram'><CiInstagram/></a>
-                <a href='' className='x'><CiTwitter /></a>
-            </div>
-            </div>
-        
-        </NavBlock>
-    );
+                <Accordion.Body>
+                  <a href="https://www.woowahan.com/" target="_blank">
+                    우아한형제들
+                  </a>
+                </Accordion.Body>
+                <Accordion.Body>
+                  <a href="https://www.deliveryhero.com/" target="_blank">
+                    deliveryhero
+                  </a>
+                </Accordion.Body>
+                <Accordion.Body>
+                  <a href="https://www.woowayouths.com/" target="_blank">
+                    우아한청년들
+                  </a>
+                </Accordion.Body>
+                <Accordion.Body>
+                  <a href="https://woowarider.or.kr/" target="_blank">
+                    우아한 라이더 살핌기금
+                  </a>
+                </Accordion.Body>
+                <Accordion.Body>
+                  <a href="https://woowasajangnim.or.kr/" target="_blank">
+                    우아한 사장님 자녀 장학금 지원
+                  </a>
+                </Accordion.Body>
+                <Accordion.Body>
+                  <a href="https://baemin.dosirak.or.kr/">배민방학도시락</a>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+        </div>
+        <div className="sns">
+          <a href="" className="facebook">
+            <CiFacebook />
+          </a>
+          <a href="" className="instargram">
+            <CiInstagram />
+          </a>
+          <a href="" className="x">
+            <CiTwitter />
+          </a>
+        </div>
+      </div>
+    </NavBlock>
+  );
 };
 
 export default Nav;
