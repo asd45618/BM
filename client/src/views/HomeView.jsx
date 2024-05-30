f06013dc3f93b7550dbcc84599950be6329949a7
unknown <tjdghqkr0615@gmail.com>
2024-05-30 18:24:26 +0900

    HomeSection1~4 완성

--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HomeSection01 from "@/components/home/HomeSection01";
 import HomeSection02 from "@/components/home/HomeSection02";
+import HomeSection03 from "@/components/home/HomeSection03";
+import HomeSection04 from "@/components/home/HomeSection04";
 
 const HomeView = () => {
   return (
     <div>
       <HomeSection01 />
       <HomeSection02 />
+      <HomeSection03 />
+      <HomeSection04 />
     </div>
   );
 };
